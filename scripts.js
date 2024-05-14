9f6eb6387c3ed6bf85d461ad931b378edc59e85b
shelby52 <152772703+shelby52@users.noreply.github.com>
2024-05-15 02:46:11 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+document.getElementById('menu-button').addEventListener('click',function(){
+    document.getElementById('dropdown').classList.toggle('open');
+})
+document.getElementById('ham').addEventListener('click',function(){
+    document.getElementById('dropdown').classList.toggle('open')
+})
+function sphones(){
+    window.location.href = "sphones.html";
+}
+function tabs(){
+    window.location.href = "tablets.html";
+}
+function watch(){
+    window.location.href = "watch.html";
+}
+function ten(){
+    window.location.href = "top.html";
+}
